ff1987d7443da77d2dff5171a1665a2e18f6388c
a-abdurrahman <aa.abdurrahman03@gmail.com>
2024-12-02 17:42:25 +0700

    owners table

---
+++ migrations/1733135714435_create-owner-table.js
@@ -0,0 +1,38 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("owners", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    username: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    email: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    password: {
+        type: 'TEXT',
+        notNull: true,
+    },
+    business_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable("owners");
+};
