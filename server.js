5eddfe239899d9e9a5e2dd859c93c9e5e0438fd6
a-abdurrahman <aa.abdurrahman03@gmail.com>
2024-12-02 17:10:27 +0700

    exception handlers

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 require('@hapi/hoek')
 const Hapi = require('@hapi/hapi');
 const { routes } = require('./src/route/routes');
+const ClientError = require('./src/exceptions/ClientError')
 
 const init = async () => {
     
@@ -11,6 +12,20 @@ const init = async () => {
         port: process.env.HAPI_PORT,
     })
     server.route(routes);
+    server.ext('onPreResponse', (request, h) => {
+        const { response } = request;
+    
+        if (response instanceof ClientError) {
+          const newResponse = h.response({
+            status: 'fail',
+            message: response.message,
+          });
+          newResponse.code(response.statusCode);
+          return newResponse;
+        }
+    
+        return h.continue;
+      });
 
     await server.start();
     console.log(`Server started on: ${server.info.uri}`);
