53df9785b238951c2fe2e6b91686bba1cfd91f16
a-abdurrahman <aa.abdurrahman03@gmail.com>
2024-12-04 18:38:30 +0700

    validation edit

--- src/validator/owner/schema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const OwnersPayloadSchema = Joi.object({
-  username: Joi.string().required(),
-  email: Joi.string().email({ minDomainSegments: 2 }).required(),
-  password: Joi.string().required(),
-});
-
-module.exports = { OwnersPayloadSchema };
